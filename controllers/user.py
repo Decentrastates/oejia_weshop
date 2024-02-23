685010681a8c87ac9b6a674118f8bae1a133f592
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2024-02-23 11:56:32 +0800

    注册的partner默认为客户类型

--- controllers/user.py
+++ controllers/user.py
@@ -154,6 +154,7 @@ class WxappUser(http.Controller, BaseController):
                 'partner_id': user_id and request.env['res.users'].sudo().browse(user_id).partner_id.id or None,
                 'category_id': [(4, request.env.ref('oejia_weshop.res_partner_category_data_1').sudo().id)],
                 'entry_id': entry.id,
+                'customer_rank': 1,
             }
             if user_id:
                 vals['user_id'] = user_id
